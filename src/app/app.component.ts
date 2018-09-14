b1ab9c53403f599968780b6a47a03286338ffac7
Danh Nguyen <danngu@wapice.localdomain>
2018-09-14 12:06:04 +0300

    after wp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'course-project';
+export class AppComponent implements OnInit {
+  loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyA8VEaDWHjrG-sejoB_GXLpfhQvmhVhJeg',
+      authDomain: 'ngcoursepj.firebaseapp.com',
+      databaseURL: 'https://ngcoursepj.firebaseio.com',
+      projectId: 'ngcoursepj',
+      storageBucket: 'ngcoursepj.appspot.com',
+      messagingSenderId: '356349711426'
+    });
+  }
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
+import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    AppRoutingModule,
+    SharedModule,
+    ShoppingListModule,
+    AuthModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
